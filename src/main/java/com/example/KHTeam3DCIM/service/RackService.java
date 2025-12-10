a95c50695a8cdfa8fc800bdd661233328f475daf
csh6170 <csh6170@gmail.com>
2025-12-10 15:34:18 +0900

    member rack 서비스 추가 및 device 서비스 주석 제거

--- src/main/java/com/example/KHTeam3DCIM/service/RackService.java
+++ src/main/java/com/example/KHTeam3DCIM/service/RackService.java
@@ -1,4 +1,44 @@
 package com.example.KHTeam3DCIM.service;
 
+import com.example.KHTeam3DCIM.domain.Rack;
+import com.example.KHTeam3DCIM.repository.RackRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class RackService {
+
+    private final RackRepository rackRepository;
+
+    // 1. 전체 랙 조회
+    public List<Rack> findAllRacks() {
+        return rackRepository.findAll();
+    }
+
+    // 2. 랙 아이디로 조회
+    public Optional<Rack> findById(Long rackId) {
+        return rackRepository.findById(rackId);
+    }
+
+    // 3. 랙 추가
+    public Rack addRack(Rack rack) {
+        return rackRepository.save(rack);
+    }
+
+    // 4. 랙 수정
+    public Rack updateRack(Rack rack) {
+        // 필요시 존재 여부 확인 후 수정
+        return rackRepository.save(rack);
+    }
+
+    // 5. 랙 삭제
+    public void deleteRack(Long rackId) {
+        rackRepository.deleteById(rackId);
+    }
 }
