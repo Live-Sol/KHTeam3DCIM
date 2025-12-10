a312c0083583ee8dd6ca2b39d3f311e2340c6fb5
csh6170 <csh6170@gmail.com>
2025-12-10 11:56:04 +0900

    first commit

---
+++ src/main/java/com/example/KHTeam3DCIM/KhTeam3DcimApplication.java
@@ -0,0 +1,13 @@
+package com.example.KHTeam3DCIM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhTeam3DcimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhTeam3DcimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KHTeam3DCIM/KhTeam3DcimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KHTeam3DCIM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhTeam3DcimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
